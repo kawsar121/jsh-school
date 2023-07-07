743b9324738ef349e90865e8bbe2eb7df1cff015
kawsar121 <tohidulislamkawsarbhuiyan2567gmail.com>
2023-07-06 18:21:46 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,412 @@
+/*var bagina = "tahmid";
+// var number_cheek = bagina.length;
+document.write("total number is =" + bagina.length) */
+
+
+/*var bagina = prompt("Enter your name")
+document.write("the character of the number is :" + bagina.length)           */
+
+
+// this problem not solbe , so i can try it latter
+/*
+var number1 = 10
+var number2 = 5
+var sub;
+sub =  number1 - number2;
+document.write(sub)
+*/
+
+/*
+var text = "bangladesh";
+text = text.toUpperCase();
+document.write(text)
+*/
+/*
+var text1 = "bangladesh";
+var text2 = "is a beautiful country";
+var text = text1.concat(text2);
+document.write(text)
+*/
+
+
+
+/*
+var name1 = prompt("Enter Your First Namne");
+var name2 = prompt("Enter Your First Namne");
+document.write(name1 + name2)
+*/
+/*
+ var text = "bangladesh";
+ var abul = text.toUpperCase();
+ document.write(abul)
+*/
+
+/*
+var number1 = 20;
+var number2 = 10;
+var sum;
+sum = number1 + number2;
+document.write( "the sum is" + sum)
+*/
+/*
+var number1 = prompt("Enter your first number" );
+var number2 = prompt("nter your second number" );
+var number1 = parseInt(number1, 10);
+var number2 = parseInt(number2, 10);
+
+var sum;
+sum = number1 + number2;
+document.write(number1 + "+" + number2 + "="  + sum )
+*/
+
+/* Square
+
+var vase = prompt("Enter submit your vase");
+var height = prompt("Enter submit your height");
+var area = vase*height ;
+document.write("Your full area is :" + area)
+
+*/
+
+/* Rectungler
+
+var vase = prompt("Enter submit your vase");
+var height = prompt("Enter submit your height");
+var area = (vase*height) / 2 ;
+document.write("Your full area is :" + area)
+
+*/
+
+
+
+/* Square
+
+var vase = prompt("Enter submit your vase");
+var height = prompt("Enter submit your height");
+var area = vase*height ;
+document.write("Your full area is :" + area)
+
+*/
+
+/* celsious
+var far = prompt("Enter submit your number");
+
+var cel = (far-32)*(5/9);
+document.write("Your anser is :" + cel)
+*/
+
+/*
+
+var cel = prompt("Enter submit your number");
+
+var far = (cel*(9/5))+32;
+document.write("Your answer is :" + far);
+
+*/
+/*
+var num1 = prompt("Enter your number");
+var num2 = prompt("Enter your number");
+
+if (num1 > num2)
+    console.log("positive")
+else if (num1 < num2)
+    console.log("negitive")
+if (num1 == num2)
+    console.log("zero")
+*/
+/*ekhane function te moner khushi document.write o kora jabe amne o kora jabe
+
+var num1 = prompt("Enter your first number");
+var num2 = prompt("Enter your second number");
+
+if (num1 > num2)
+    document.write("positive")
+else if (num1 < num2)
+    document.write("negitive")
+else (num1 == num2)
+    document.write("zero")
+*/
+
+
+/*
+var Marks = prompt("Enter submit your marks");
+if(Marks >=80)
+    document.write("A+");
+else if(Marks >=70)
+    document.write("A");
+else if(Marks >=60)
+    document.write("A-");
+else if(Marks >=50)
+    document.write("B");
+else if(Marks >=40)
+    document.write("c");
+else if(Marks >=33)
+    document.write("D");
+else
+    document.write("Fail");
+
+*/
+
+/*
+
+var letter = prompt("Enter your letter");
+
+if(letter=="a" || letter=="e" || letter=="i" || letter=="o" || letter=="u")
+    document.write("vowel");
+else
+    document.write("consonent");
+
+
+           (ami saile  2nd programme e uppercase e likhte pari , no problem, because programme ar converte this lowwercase)
+
+var letter = prompt("Enter your letter");
+letter = letter.toLowerCase();
+if(letter=="a" || letter=="e" || letter=="i" || letter=="o" || letter=="u")
+    document.write("vowel");
+else
+    document.write("consonent");
+
+*/
+
+
+
+
+
+//patta na dile amon hobe
+
+
+    // var letter = prompt("Tumi ki amako valobasho");
+    // letter = letter.toLowerCase();
+    // if(letter=="yes" || letter=="hm" || letter=="obbosoi" || letter=="tomake sada ar kake" || letter=="tumi amar shob")
+    //     document.write("solo biye kori feli halal");
+    // else
+    //     document.write("Dur h samne theke, meyr ki obab ase");
+
+    /*
+        
+var number = prompt("Enter your digits");
+    switch(number){
+        case"0":
+    document.write("zero");
+        break;
+        case"1":
+    document.write("one");
+        break;
+        case"2":
+    document.write("two");
+        break;
+        case"3":
+        document.write("three");
+            break;
+            case"4":
+            document.write("four");
+                break;
+                case"5":
+    document.write("five");
+        break;
+        case"6":
+        document.write("six");
+            break;
+            case"7":
+            document.write("seven");
+                break;
+                case"8":
+                document.write("eight");
+                    break;
+                    default:
+                    document.write("not found")
+    }
+    */
+/*
+var letter = prompt("Enter your letter");
+switch(letter){
+    case"a":
+    document.write("vaowel")
+    break;
+    case"e":
+    document.write("vaowel")
+    break;
+    case"i":
+    document.write("vaowel")
+    break;
+    case"o":
+    document.write("vaowel")
+    break;
+    case"u":
+    document.write("vaowel")
+    break;
+    default:
+    document.write("not found your letter")
+}
+*/
+// eta avar akline o saile kora jaw
+/*
+var letter = prompt("Enter your letter");
+letter = letter.toLowerCase;
+switch(letter){
+    case"a":
+    case"e":
+    case"i":
+    case"o":
+    case"u":
+    document.write("vaowel")
+    break;
+    default:
+        document.write("consonent")
+}
+*/
+
+// for (var x = 10; x >= 1; x = x-1){
+//     document.write(" " + x);
+// }
+
+            //for loop
+
+/* bijor shonkhar programme
+
+for (var x=1; x<=99; x=x+2)  
+{
+    document.write(" " +x)
+}
+*/
+/* jor shonkhar programme
+for( var x = 2 ; x<=100 ; x=x+2)
+{
+    document.write(" " +x)
+}
+*/
+/* ulta solbe jemon 10 9 8 7 ect
+for( var x = 10 ; x>=1 ; x=x-1)
+{
+    document.write(" "+x)
+}
+*/
+
+// var sum =0;
+// for( var x =1; x<=10; x=x+1)
+// {
+//     sum = sum+x;
+// }
+// document.write(sum)
+
+/*
+var m = parseInt(prompt("Enter your first number"))
+var n = parseInt(prompt("Enter your second number"))
+var sum =0;
+for( var x =m; x<=n; x=x+1)
+{
+    sum = sum+x;
+}
+document.write(sum)
+*/
+
+
+
+
+
+                            //while loop
+
+
+
+/*
+var x = 1;
+var sum = 0;
+while(x <= 10)
+{
+    sum = sum + x;
+    x= x+1;
+}
+document.write(sum)
+*/
+
+/* 1 theke 100 jor shonkhar programme
+var x =2;
+var sum =0;
+while(x<=100)
+{
+    sum = sum + x;
+    x = x+2;
+}
+document.write(sum)
+*/
+
+/*
+//1-100 porjontpo shonkha gula ke 3 and 5 dara bag korle tadeer jogpo
+
+var x =1;
+var sum =0;
+while(x<=20)
+{
+
+if(x%3==0 && x%5==0)
+
+    sum = sum + x;
+    x = x+1;
+}
+document.write(sum)
+*/
+
+// var x=1;
+// while(x<=10) 
+   
+// {
+//     document.write(" " +x)
+//     x=x+1;
+// }
+
+
+// var x=1; 
+   
+// do {
+//     document.write(" " +x)
+//     x=x+1;
+// }
+// while(x<=10)
+
+// function addition (x1,x2)
+// {
+//     var result = x1 + x2;
+//     document.write("sum is =" + result + "<br>")
+// }
+
+// function subtruction (x1,x2)
+// {
+//     var result = x1 - x2;
+//     document.write("sum is =" + result + "<br>")
+// }
+
+// addition(10,30);
+// addition(10,20);
+// subtruction(50,40);
+
+
+
+/*
+var number = prompt("enter your number")
+if(number>=80)
+    document.write("A+")
+else if(number>=70)
+    document.write("A")
+else if(number>=60)
+    document.write("A-")
+else if(number>=50)
+    document.write("B")
+else if(number>=40)
+    document.write("C")
+else if(number>=33)
+    document.write("D")
+else
+    document.write("Fail")
+*/
+/*
+var number1 = parseInt(prompt("Enter Your First Number"))
+var number2 = parseInt(prompt("Enter Your second Number"))
+var sum = number1+number2
+document.write(sum)
+*/
+
+// var names=["rinku","tinku","sinku"]
+// names.push("jomir")
+// document.write(names)
+// document.write(names.length)
+
